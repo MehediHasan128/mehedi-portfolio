bb7aa6394dc79554cbd5005dfa25d4fb67b66515
MehediHasan128 <mehedihasan12926@gmail.com>
2023-12-18 01:28:49 +0600

    add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Components/Router/Route.jsx'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react'
+
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
