75aeb56e0290da64934775ef92760dc231e07339
MehediHasan128 <mehedihasan12926@gmail.com>
2025-06-21 01:40:18 +0600

    complete home page

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 import { AiOutlineDownload } from "react-icons/ai";
 import logo from "../../assets/logo/logo.png";
 
-const navItems = ['Home', 'About', 'Skills', 'Projects', 'Contact']
+const navItems = ['Home', 'About', 'Skills', 'Projects', 'Education', 'Contact']
 
 const Navbar = () => {
   return (
@@ -10,15 +10,15 @@ const Navbar = () => {
         <div className="w-[90%] mx-auto flex items-center justify-between">
           <div className="flex items-center gap-2 cursor-pointer">
             <img className="w-10" src={logo} alt="logo" />
-            <span className="text-2xl text-white font-bold">Mehedi</span>
+            <span className="text-2xl text-white">Mehedi</span>
           </div>
-          <div className="flex space-x-16 text-lg font-semibold text-white">
+          <div className="flex space-x-16 text-lg text-white">
             {
               navItems.map((item) => <button className="cursor-pointer">{item}</button>)
             }
           </div>
           <div>
-            <button className="px-5 py-2 rounded-md text-lg font-medium bg-white flex items-center gap-2 cursor-pointer active:scale-95 duration-1000"><AiOutlineDownload className="text-2xl" /> Resume</button>
+            <button className="px-5 py-2 rounded-md text-lg bg-white flex items-center gap-2 cursor-pointer active:scale-95 duration-1000"><AiOutlineDownload className="text-2xl" /> Resume</button>
           </div>
         </div>
       </div>
--- src/sections/home/Home.tsx
+++ src/sections/home/Home.tsx
@@ -1,17 +1,36 @@
+import { BsFillCursorFill } from "react-icons/bs";
 import banner from "../../assets/images/mehedi.png";
 
 const Home = () => {
   return (
     <div className="relative bg-radial-[at_50%_55%] from-[#4f5764] via-[#21252B] to-[#21252B] to-100% min-h-screen">
-        {/* Image */}
+      {/* Image */}
       <div className="w-[35%] absolute bottom-0 left-1/2 transform -translate-x-1/2 z-10">
-        <div>
+        <div className="relative">
           <img src={banner} alt="mehedi" />
+          <div className="absolute top-3/4 -left-12">
+            <div className="relative">
+              <BsFillCursorFill className="text-2xl absolute -top-8 -right-5 text-blue-300" />
+              <span className="bg-blue-100 px-5 py-2 font-medium rounded-b-xl rounded-tl-xl mt-2">MERN Stack Developer</span>
+            </div>
+          </div>
+          <div className="absolute top-[60%] -right-12">
+            <div className="relative">
+              <BsFillCursorFill className="text-2xl absolute -top-8 -left-5 -rotate-90 text-blue-300" />
+              <span className="bg-blue-100 px-5 py-2 font-medium rounded-b-xl rounded-tr-xl mt-2">Best Ever Specialist</span>
+            </div>
+          </div>
         </div>
       </div>
+      
       {/* Content */}
-      <div className="absolute top-[20%] left-1/2 transform -translate-x-1/2">
-        <h1 id="bannerContent" className="text-[280px] font-extrabold text-white">Portfolio</h1>
+      <div className="absolute top-[20%] left-1/2 transform -translate-x-1/2 w-full text-center">
+        <h1
+          id="bannerContent"
+          className="text-[300px] font-extrabold text-white"
+        >
+          MEHEDI PORTFOLIO
+        </h1>
       </div>
     </div>
   );
